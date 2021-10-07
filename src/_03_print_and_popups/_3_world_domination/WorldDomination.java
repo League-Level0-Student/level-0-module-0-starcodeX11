7921e48245b797664a021ac7018e4ee4a1aa3a34
student <student@ip-172-31-52-166.us-west-2.compute.internal>
2021-10-06 19:58:40 -0700

    Fun code!

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,9 +8,13 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String input = JOptionPane.showInputDialog("Do u know how 2 do code?");
 		// 2. If they say "yes", tell them they will rule the world.
-
+		if (input.equals("yes")) {
+			JOptionPane.showMessageDialog(null,"YOU GOT IT RIGHT! :D ");
+		} else {
+			JOptionPane.showMessageDialog(null,"you got it wrong! D: ");
+		}
 		// 3. Otherwise, tell them to sign up for classes at The League.
 
 	}
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,14 +1,21 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int score=0;
 		// ASK A QUESTION AND CHECK THE ANSWER
-		
+		String input = JOptionPane.showInputDialog("Are mummys real?");
 				// 2.  Ask the user a question 
-				
+		if (input.equals("yes")) {
+			JOptionPane.showMessageDialog(null,"YOU GOT IT RIGHT! :D ");
+			score=score+1;
+		} else {
+			JOptionPane.showMessageDialog(null,"you got it wrong! D: ");
+		}
 				// 3.  Use an if statement to check if their answer is correct
 				
 				// 4.  if the user's answer was correct, add one to their score 
